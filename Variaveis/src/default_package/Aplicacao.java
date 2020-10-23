59b8c689baf3b72167fd6a5c5018b01aadaff15a
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2020-10-23 13:28:25 -0300

    Trabalhando com variáveis.

---
+++ Variaveis/src/default_package/Aplicacao.java
@@ -0,0 +1,21 @@
+package default_package;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		//int numero;
+		//numero = 50;
+		
+		int numero = 50;
+		int numero2 = numero + 5;
+		
+		//numero = numero -1;
+		//numero -= 1;
+		//numero--;
+		
+		//numero = numero * 2;
+		numero *= 2;
+		
+		System.out.println(numero);
+		System.out.println(numero2);
+	}
+}
