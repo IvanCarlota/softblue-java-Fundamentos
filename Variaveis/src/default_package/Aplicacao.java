fdfc66bfbc8129175dd854e469dcccdbd80586a7
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2020-10-23 14:17:45 -0300

    Primeiro commit pelo eclipse

--- Variaveis/src/default_package/Aplicacao.java
+++ Variaveis/src/default_package/Aplicacao.java
@@ -5,17 +5,20 @@ public class Aplicacao {
 		//int numero;
 		//numero = 50;
 		
-		int numero = 50;
-		int numero2 = numero + 5;
+		//int numero = 50;
+		//int numero2 = numero + 5;
 		
 		//numero = numero -1;
 		//numero -= 1;
 		//numero--;
 		
 		//numero = numero * 2;
-		numero *= 2;
+		//numero *= 2;
 		
-		System.out.println(numero);
-		System.out.println(numero2);
+		//System.out.println(numero);
+		//System.out.println(numero2);
+		
+		boolean b = true;
+		System.out.println(b);
 	}
 }
